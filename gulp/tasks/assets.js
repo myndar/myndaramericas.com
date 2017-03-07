3194ad993b624bd084ac030b818830f2819bcf91
ryandeussing <ryan@ryandeussing.com>
2017-03-07 12:11:54 -0500

    not excluding assets, injecting

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -65,7 +65,7 @@ gulp.task('styles', () =>
       precision: 10
     }).on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer({browsers: 'last 1 version'})
+      autoprefixer({browsers: '> 5%'})
     ]))
     .pipe(size({
       showFiles: true
