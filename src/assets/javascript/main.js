c5172bb375f84e07b19b8c52ea42c3c0e9b60bce
ryandeussing <ryan@ryandeussing.com>
2017-05-04 15:01:56 -0400

    content

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,13 +1,6 @@
 (function() {
 
-
-  // visual grid: use 'g' key to toggle body class
-  var k = new Kibo();
-  k.down(['g'], function() {
-    $('body').addClass('grid');
-  }).up('g', function() {
-    $('body').removeClass('grid');
-  });
-
+  $('header a').smoothScroll({offset: -100});
+  $('a.arrow').smoothScroll({offset: -100});
 
 })();
