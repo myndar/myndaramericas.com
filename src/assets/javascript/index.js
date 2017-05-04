014427b9cd5ec0915ee52ef40bf2433fae7ce302
ryandeussing <ryan@ryandeussing.com>
2017-05-04 16:17:16 -0400

    wip

--- src/assets/javascript/index.js
+++ src/assets/javascript/index.js
@@ -10844,5 +10844,24 @@ Kibo.prototype.lastModifiersAndKey = function() {
   $('header a').smoothScroll({offset: -100});
   $('a.arrow').smoothScroll({offset: -100});
 
+  $(function() {
+    var header = $('header');
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      var siteHeader = $('.site-header').height();
+      var offset = $('.logo-trigger').offset().top;
+      var trigger = (offset - siteHeader);
+      var arrow = $('a.arrow');
+
+      if (scroll >= trigger) { // check the offset top
+        header.addClass('scrolled');
+        arrow.css('visibility', 'hidden')
+      } else { // check the scrollHeight
+        header.removeClass('scrolled');
+        arrow.css('visibility', 'visible')
+      }
+    });
+  });
+
 })();
 //# sourceMappingURL=index.js.map
--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -3,4 +3,23 @@
   $('header a').smoothScroll({offset: -100});
   $('a.arrow').smoothScroll({offset: -100});
 
+  $(function() {
+    var header = $('header');
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      var siteHeader = $('.site-header').height();
+      var offset = $('.logo-trigger').offset().top;
+      var trigger = (offset - siteHeader);
+      var arrow = $('a.arrow');
+
+      if (scroll >= trigger) { // check the offset top
+        header.addClass('scrolled');
+        arrow.css('visibility', 'hidden')
+      } else { // check the scrollHeight
+        header.removeClass('scrolled');
+        arrow.css('visibility', 'visible')
+      }
+    });
+  });
+
 })();
